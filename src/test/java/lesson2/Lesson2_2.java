f821a2aacead2e9e5faf2e72b2df9a1a6a1fb8f8
a.dolzhonok <A.Dolzhonok@m-developer.ru>
2024-03-15 21:04:46 +0300

    remove if constructions

--- src/test/java/lesson2/Lesson2_2.java
+++ src/test/java/lesson2/Lesson2_2.java
@@ -1,16 +1,21 @@
 package lesson2;
 
 public class Lesson2_2 {
-    public void checkSumSign() {
-        int a = 15;
-        int b = -20;
-        int sum = (a + b);
-        if (sum >= 0) System.out.println("Сумма положительная");
-        if (sum <= 0) System.out.println("Сумма отрицательная");
-    }
 
-    public static void main(String[] args) {
-        Lesson2_2 print = new Lesson2_2();
-        print.checkSumSign();
+  int a = 15;
+  int b = -20;
+
+  public void checkSumSign() {
+    int sum = (a + b);
+    if (sum >= 0) {
+      System.out.println("Сумма положительная");
+    } else {
+      System.out.println("Сумма отрицательная");
     }
+  }
+
+  public static void main(String[] args) {
+    Lesson2_2 print = new Lesson2_2();
+    print.checkSumSign();
+  }
 }
--- src/test/java/lesson2/Lesson2_4.java
+++ src/test/java/lesson2/Lesson2_4.java
@@ -1,16 +1,22 @@
 package lesson2;
 
 public class Lesson2_4 {
-    public void compareNumbers() {
-        int a = 101;
-        int b = 20;
-        if (a >= b) System.out.println("a>=b");
-        else System.out.println("a<=b");
 
+  int a = 101;
+  int b = 20;
+
+  public void compareNumbers() {
+
+    if (a >= b) {
+      System.out.println("a>=b");
+    } else {
+      System.out.println("a<=b");
     }
 
-    public static void main(String[] args) {
-        Lesson2_4 print = new Lesson2_4();
-        print.compareNumbers();
-    }
+  }
+
+  public static void main(String[] args) {
+    Lesson2_4 print = new Lesson2_4();
+    print.compareNumbers();
+  }
 }
